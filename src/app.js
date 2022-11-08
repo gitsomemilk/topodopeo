48d1efc1fa5f999d10618d4d8c4630d61a8e976a
gitsomemilk <m.d.bood@hotmail.com>
2022-11-08 17:45:39 +0100

    first push

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 console.log('Hallo daar!');
+import axios from 'axios';
+
+ async function fetchData(){
+  const URI = 'https://restcountries.com/v2/all/'
+
+
+
+  try{
+   const response = await axios.get ( URI );
+   console.log(response);
+  } catch (error){
+   console.error(error)
+  }
+
+ }
+fetchData()
