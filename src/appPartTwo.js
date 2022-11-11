2e31707c20412142152f1cd884f3638b44cd5149
gitsomemilk <m.d.bood@hotmail.com>
2022-11-11 18:16:37 +0100

    part 2 script klaar

--- src/appPartTwo.js
+++ src/appPartTwo.js
@@ -1,33 +1,68 @@
 import axios from "axios";
 
-const placeSingleCountry = singlecountry => {
-    const singleCountry =document.getElementById('partTwoArticle');
-    const singleCountryFlag = document.createElement('img');
-    const singleCountryName = document.createElement('li');
-    const singleCountrySubArea = document.createElement('li');
-    const singleCountryPopulation = document.createElement('li');
-    const singleCountryCapitol = document.createElement('li');
-    const singleCountryCurrency = document.createElement('li');
+const countryBlock = document.getElementById('country-block');
+const searchField = document.getElementById('searchBar');
+const errorMessage = document.getElementById('error-message');
+const searchButton = document.getElementById('searchButton');
 
-    // countryFlags maken
+const checkCurrency = currencies => {
+    const currenciesArray = currencies.map(currency => currency.name)
+    return currenciesArray.join("'s and ");
+
+}
+
+const placeSingleCountry = singlecountry => {
+
+    const singleCountryFlag = document.getElementById('country-flag');
+    const singleCountryName = document.getElementById('country-name');
+    const countryInfo = document.getElementById('country-info');
+
+    const {flags, name, subregion, population, capital, currencies} = singlecountry;
+
+    // singleCountryFlags maken
 
     singleCountryFlag.setAttribute('src', flags.png);
-    singleCountryFlag.classList.add("countryFlag");
-    singleCountryFlag.appendChild(singleCountryFlag);
+
+    // singelCountryName maken
+    singleCountryName.textContent = name;
+
+
+    //countryInfo maken
+    countryInfo.textContent = `${name} is situated in ${subregion}.it has a population of ${population} people, The capital is ${capital} and you can pay with ${checkCurrency(currencies)}'s`;
+
+    countryBlock.style.display="block"
+
+
 }
 
 
 // Async Functions voor part 2
-async function fetchSpecificData(){
-    const URISpecific ='https://restcountries.com/v2/name/'
-    let inputSearch = "netherlands"
+async function fetchSpecificData() {
+    const URISpecific = 'https://restcountries.com/v2/name/'
+    let inputSearch = searchField.value
+
+    errorMessage.style.display = "none"
+    countryBlock.style.display = "none"
+    searchField.value= ''
     try {
-        const specificData= await axios.get(URISpecific + inputSearch )
+        const specificData = await axios.get(URISpecific + inputSearch)
         console.log(specificData);
-    }catch(e){
+        placeSingleCountry(specificData.data[0])
+
+    } catch (e) {
+        errorMessage.style.display= "block"
         console.error(e);
     }
 
 }
 
-fetchSpecificData()
+// button aanroepen
+
+
+// addeventlistener aanmaken
+searchButton.addEventListener('click' ,fetchSpecificData)
+searchField.addEventListener('keypress',(e)=>{
+    if (e.key=== 'Enter'){
+        fetchSpecificData()
+    }
+})
